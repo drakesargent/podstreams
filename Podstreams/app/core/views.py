f1ff73759c2a026b40a6a69570f6d19dea4ff4a4
Kenneth Drake-Sargent <kennethrsargent@gmail.com>
2019-01-15 12:06:38 -0800

    refactored design for constraints / route frame

--- Podstreams/app/core/views.py
+++ Podstreams/app/core/views.py
@@ -8,3 +8,23 @@ rue = Blueprint('core',__name__)
 def index():
     print("accessed root")
     return "<h1>Hello World</h1>"
+
+@rue.route('/a/')
+def auth():
+    print("authorized")
+    return "<h1>Authorized</h1>"
+
+@rue.route('/a/add/')
+def add():
+    print("add")
+    return "<h1>Add Form</h1>"
+
+@rue.route('/a/update/')
+def update():
+    print("update")
+    return "<h1>Update</h1>"
+
+@rue.route('/a/delete/')
+def delete():
+    print("delete")
+    return "<h1>Delete</h1>"
